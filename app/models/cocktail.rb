f6618b57971054812ea9ee35a25e6eb38e643c38
Poppy Jennings <popjen@hotmail.com>
2021-03-07 16:34:45 +0100

    updated styling

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,4 +3,8 @@ class Cocktail < ApplicationRecord
   has_many :ingredients, through: :doses
   validates :name, presence: true, uniqueness: true
   has_one_attached :photo
+  has_one_attached :bartender
+  validates :photo, presence: true
+  validates :bartender, presence: true
+  # validates :bartenderName, presence: true
 end
